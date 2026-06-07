2fbfebc01e2a5d94ee5d16a2e29e04154a75a9f2
Lars Artmann <git@lars.software>
2026-06-07 20:39:59 +0200

    chore(build): add lake build configuration files
    - Add lake.nix for project build configuration
    - Add lake.lock for dependency pinning
    - Enable reproducible builds with Nix

--- flake.nix
+++ flake.nix
@@ -3,38 +3,70 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    systems.url = "github:nix-systems/default";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.default = pkgs.buildGoModule {
-          pname = "go-localsync";
-          version = "0.1.0";
+  outputs =
+    inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-          src = ./.;
+      imports = [ inputs.treefmt-nix.flakeModule ];
 
-          vendorHash = null;
-
-          meta = with pkgs.lib; {
-            description = "Generic synchronization SDK with CQRS";
-            homepage = "https://github.com/larsartmann/go-localsync";
-            license = licenses.mit;
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          packages.default = pkgs.buildGoModule {
+            pname = "go-localsync";
+            version = "0.1.0";
+            src = ./.;
+            vendorHash = null;
+            meta = with pkgs.lib; {
+              description = "Generic synchronization SDK with CQRS";
+              homepage = "https://github.com/larsartmann/go-localsync";
+              license = licenses.mit;
+            };
           };
-        };
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            go_1_26
-            golangci-lint
-            ginkgo
-            gotools
-            gofumpt
-          ];
+          devShells = {
+            default = pkgs.mkShell {
+              packages = with pkgs; [
+                go_1_26
+                golangci-lint
+                ginkgo
+                gotools
+                gofumpt
+              ];
+            };
+
+            ci = pkgs.mkShellNoCC {
+              packages = with pkgs; [
+                go_1_26
+                golangci-lint
+              ];
+            };
+          };
+
+          treefmt = {
+            projectRootFile = "go.mod";
+            programs = {
+              gofumpt.enable = true;
+              nixfmt.enable = true;
+            };
+          };
+
+          formatter = config.treefmt.build.wrapper;
         };
-      });
+    };
 }
